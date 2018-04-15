8705da0c439d6a597bf481a2fcd9a0c4cf204831
Nishant Uniyal <nishantuniyal@gmail.com>
2018-04-14 21:58:44 -0700

    update publish script

--- publish_website.sh
+++ publish_website.sh
@@ -19,7 +19,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-../bin/hugo
+hugo
 
 echo "Updating master branch"
 cd public && git add --all && git commit -m "Publishing website (publish_website.sh)" && git push
